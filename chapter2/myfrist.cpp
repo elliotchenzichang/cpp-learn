7c5469422d53ed60c8f1b610695bf03f73a244e9
Elliot Chen <92676541+elliotchenzichang@users.noreply.github.com>
2023-01-22 19:49:19 +0800

    add demo code

--- chapter2/myfrist.cpp
+++ chapter2/myfrist.cpp
@@ -7,5 +7,8 @@ int main()
     cout << " Come up and C++ me some time.";
     cout << endl;
     cout << " You won't regret it!" << endl;
+    int numbers;
+    cin >> numbers;
+    cout << "value of numbers after input is " << numbers << endl;
     return 0;
 }
