485bcd02a4f8c74044dacae25ce35cb9801056bb
Elliot Chen <92676541+elliotchenzichang@users.noreply.github.com>
2024-01-08 20:31:04 +0800

    add demo3 for add files in a dir

---
+++ demo3/math/CMakeFiles/powerFunction.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/powerFunction.dir/powerFunction.cc.o"
+  "CMakeFiles/powerFunction.dir/powerFunction.cc.o.d"
+  "libpowerFunction.a"
+  "libpowerFunction.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/powerFunction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
